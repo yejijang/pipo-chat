b1803a1c7b46b6fde41cf841538461cf4276fba8
yejijang <yeji3532@gmail.com>
2021-01-02 21:11:07 +0900

    최초 등록

---
+++ src/main/java/com/pipochat/PipoApplication.java
@@ -0,0 +1,13 @@
+package com.pipochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipoApplication.class, args);
+	}
+
+}
